8c19741c587c72cedca045e676fa50d640496dd4
jyanev01 <jordanyanev@gmail.com>
2021-09-16 18:40:00 -0400

    add event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonE1 = document.querySelector("#save-task");
+formE1.addEventListener("submit",CreateTaskHandler);
 var taskToDoE1 = document.querySelector("#tasks-to-do");
 
 
 var CreateTaskHandler = function () {
+
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-itme";
     listItemE1.textContent = "This is a new task.";
     taskToDoE1.appendChild(listItemE1);
 }
 
-buttonE1.addEventListener("clicK", CreateTaskHandler);
+formE1.addEventListener("clicK", CreateTaskHandler);
