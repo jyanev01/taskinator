8f9a7371bb63250db502e4e3ef4cc4c63e38c860
jyanev01 <jordanyanev@gmail.com>
2021-09-17 10:46:06 -0400

    corrected taskFormHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
-    var taskNameInput = document.querySelector("Input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("Input[name='task-type']").value;
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // package up data as an object
+    // check if inpust are empty (validate)
+    if(taskNameInput ==="" || taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    // reset form field for next task to be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
@@ -29,8 +40,10 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.innerHTML ="<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
+    console.dir(listItemEl);
+
     // add entire list itme to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("clicK", taskFormHandler);
+formEl.addEventListener("submit", taskFormHandler);
